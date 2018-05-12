4e485b9be5dc6a1a2d814745abcb37b43f10bcc0
Józef Bongowski <j.bongowski@protonmail.com>
2018-05-12 15:45:22 +0200

    Add comments to the script

--- whereIsMyVote.R
+++ whereIsMyVote.R
@@ -1,10 +1,18 @@
-for (i in seq(length(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Lp))){
-  if (Dane_glosowania_cz_I_97075_www_gdansk_pl_$`2 cyfry PESELU`[i] == "95" && Dane_glosowania_cz_I_97075_www_gdansk_pl_$Płeć[i] == "M" && Dane_glosowania_cz_I_97075_www_gdansk_pl_$Wiek[i] == "22" && Dane_glosowania_cz_I_97075_www_gdansk_pl_$Dzielnica[i] == "Śródmieście"){
-    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Lp[i])
-    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$`Nr głosu`[i])
-    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$`Dta oddania głosu`[i])
-    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Projekt[i])
-    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Punkty[i])
+# main loop
+
+for (i in seq(length(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Lp))){ # loop through the whole file
+
+  # if undermentioned conditions are fulfil
+  if (Dane_glosowania_cz_I_97075_www_gdansk_pl_$`2 cyfry PESELU`[i] == "95" && # two first PESEL numbers are 95
+  Dane_glosowania_cz_I_97075_www_gdansk_pl_$Płeć[i] == "M" && # gender is male
+  Dane_glosowania_cz_I_97075_www_gdansk_pl_$Wiek[i] == "22" && # age is 22
+  Dane_glosowania_cz_I_97075_www_gdansk_pl_$Dzielnica[i] == "Śródmieście"){ # district is central city
+
+    # show me undermentioned data
+    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Lp[i]) # position number
+    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$`Nr głosu`[i]) # vote number
+    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$`Dta oddania głosu`[i]) # voting date
+    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Projekt[i]) # project name
+    print(Dane_glosowania_cz_I_97075_www_gdansk_pl_$Punkty[i]) # granted points
   }
 }
-
